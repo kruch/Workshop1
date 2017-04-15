71459f855f47cc61a07b182a4e1221711c9bfce7
Adrian Kruszynski <adrian.kruszynski@gmail.com>
2017-04-15 19:03:36 +0200

    zad 1 - workshop

--- zadanie1/index.php
+++ zadanie1/index.php
@@ -1,9 +1,26 @@
 <?php
-
-$arr=[];
-
-for ($i = 0; $i < 100; $i++) {
-    if($i%2==0){
-        $arr[]=$i;
+var_dump($_POST);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['name'])) {
+        echo 'Cześć, ' . $_POST['name'] . "!" . '<br>';
     }
 }
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Zadanie 1</title>
+    </head>
+    <body>
+        <form name="name" method="post">
+
+            <input type="text" name="name" placeholder="name">
+            <input type="submit" value="submit">
+
+        </form>
+    </body>
+</html>
--- zadanie2/index.php
+++ zadanie2/index.php
@@ -1,1 +1,30 @@
 <?php
+var_dump($_POST);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['name'])) {
+      
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Zadanie 2</title>
+    </head>
+    <body>
+        <form name="lotto" method="post">
+            <input type="number" name="num1" placeholder="num1">
+            <input type="number" name="num2" placeholder="num2">
+            <input type="number" name="num3" placeholder="num3">
+            <input type="number" name="num4" placeholder="num4">
+            <input type="number" name="num5" placeholder="num5">
+            <input type="number" name="num6" placeholder="num6">
+            <input type="submit" value="submit">
+
+        </form>
+    </body>
+</html>
